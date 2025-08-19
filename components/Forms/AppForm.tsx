978bab6421114cc1db93d1b3fe267b44d10541a5
nana0sei <frimpongnanao@gmail.com>
2025-08-19 14:09:43 +0000

    removed unused imports

--- components/Forms/AppForm.tsx
+++ components/Forms/AppForm.tsx
@@ -2,12 +2,11 @@ import { ReactNode } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 interface Props {
-  initialValues: any;
   onSubmit: () => void;
   children: ReactNode;
 }
 
-const AppForm = ({ initialValues, onSubmit, children }: Props) => {
+const AppForm = ({ onSubmit, children }: Props) => {
   const methods = useForm();
   return <FormProvider {...methods}>{children}</FormProvider>;
 };
