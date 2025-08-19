1021eb5fed6e9b8e45bc76397630d92c9077070b
nana0sei <frimpongnanao@gmail.com>
2025-08-19 14:21:47 +0000

    adding assets for the nav page

---
+++ app/login.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello i am login screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
