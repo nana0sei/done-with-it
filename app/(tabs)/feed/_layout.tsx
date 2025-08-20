380b6b04426659953f5d2f509b906a6c520fbba5
nana0sei <frimpongnanao@gmail.com>
2025-08-20 16:22:41 +0000

    refactor - nested stack for feed

---
+++ app/(tabs)/feed/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const FeedTabLayout = () => {
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
+
+export default FeedTabLayout;
