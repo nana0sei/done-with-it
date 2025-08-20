21c0196aa153db853b7765523cf84775c957f58c
nana0sei <frimpongnanao@gmail.com>
2025-08-20 15:17:28 +0000

    messages tab

--- app/(tabs)/account/messages.tsx
+++ app/(tabs)/account/messages.tsx
@@ -1,22 +1,55 @@
+import ListItem from "@/components/ListItem";
+import ListItemDeleteAction from "@/components/ListItemDeleteAction";
+import ListItemSeparator from "@/components/ListItemSeparator";
 import Screen from "@/components/Screen";
 import colors from "@/config/colors";
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text } from "react-native";
 
 const MessagesPage = () => {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+  const handleDelete = (message: any) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
   return (
-    <Screen style={styles.screen}>
-      <Text>messages</Text>
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        refreshing={refreshing}
+        onRefresh={() => setMessages([initialMessages[0]])}
+        renderItem={({ item }) => (
+          <ListItem
+            onPress={() => console.log("Selected", item)}
+            title={item.title}
+            subtitle={item.description}
+            image={item.image}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={() => <ListItemSeparator />}
+      />
     </Screen>
   );
 };
 
 export default MessagesPage;
 
-const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: colors.light,
-    justifyContent: "center",
-    alignItems: "center",
+const initialMessages = [
+  {
+    id: 1,
+    title: "Mosh",
+    description: "Hi Nana!",
+    image: require("@/assets/images/mosh.jpg"),
   },
-});
+  {
+    id: 2,
+    title: "Hamedani",
+    description: "Is this still available?",
+    image: require("@/assets/images/mosh.jpg"),
+  },
+];
