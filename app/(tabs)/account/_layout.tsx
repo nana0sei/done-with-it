038d876beeb8649b1fedc51ff6fe853a8459d835
nana0sei <frimpongnanao@gmail.com>
2025-08-20 12:49:38 +0000

    stack fix

--- app/(tabs)/account/_layout.tsx
+++ app/(tabs)/account/_layout.tsx
@@ -2,7 +2,15 @@ import { Stack } from "expo-router";
 import React from "react";
 
 const AccountTabLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "Account" }} />
+      <Stack.Screen
+        name="messages"
+        options={{ headerShown: true, title: "Messages" }}
+      />
+    </Stack>
+  );
 };
 
 export default AccountTabLayout;
