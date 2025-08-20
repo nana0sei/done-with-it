f8e951dd1adca5c61a81b1b21ced3548146dd9ce
nana0sei <frimpongnanao@gmail.com>
2025-08-20 11:22:39 +0000

    minor update

--- app/(tabs)/account/_layout.tsx
+++ app/(tabs)/account/_layout.tsx
@@ -1,15 +1,12 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
 
-const _layout = () => {
+const AccountTabLayout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="account" />
+      <Stack.Screen name="messages" options={{ headerShown: true }} />
     </Stack>
   );
 };
 
-export default _layout;
-
-const styles = StyleSheet.create({});
+export default AccountTabLayout;
--- app/(tabs)/account/messages.tsx
+++ app/(tabs)/account/messages.tsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Screen from "@/components/Screen";
+import colors from "@/config/colors";
 
 const MessagesPage = () => {
   return (
-    <View>
-      <Text>i am messages</Text>
-    </View>
+    <Screen style={styles.screen}>
+      <Text>messages</Text>
+    </Screen>
   );
 };
 
 export default MessagesPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.light,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
