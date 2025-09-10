6c0f0ecaff893cdf2f47241dd2c819b8ede4518d
nana0sei <frimpongnanao@gmail.com>
2025-09-10 09:57:44 +0000

    offline indicator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import OfflineNotice from "@/components/OfflineNotice";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <ThemeProvider value={lightTheme}>
+          <OfflineNotice />
           <Stack screenOptions={{ headerShown: false }} />
           <StatusBar style="auto" />
         </ThemeProvider>
---
+++ components/OfflineNotice.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import AppText from "./AppText";
+import colors from "@/config/colors";
+import Constants from "expo-constants";
+import { useNetInfo } from "@react-native-community/netinfo";
+
+const OfflineNotice = () => {
+  const { type, isInternetReachable } = useNetInfo();
+
+  if (type !== "unknown" && isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <AppText style={styles.text}>No Internet Connection</AppText>
+      </View>
+    );
+
+  return null;
+};
+
+export default OfflineNotice;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    height: 50,
+    justifyContent: "center",
+    position: "absolute",
+    top: Constants.statusBarHeight,
+    width: "100%",
+    zIndex: 1,
+  },
+  text: {
+    color: colors.white,
+  },
+});
